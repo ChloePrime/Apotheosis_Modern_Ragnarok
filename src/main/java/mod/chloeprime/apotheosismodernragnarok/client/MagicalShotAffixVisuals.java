1128e278f3568b7a9a65f4807ef1196d58bdb9f1
ChloePrime <chloe_von_einzbern@qq.com>
2024-09-01 14:19:06 +0800

    优化魔法粒子的实现

--- src/main/java/mod/chloeprime/apotheosismodernragnarok/client/MagicalShotAffixVisuals.java
+++ src/main/java/mod/chloeprime/apotheosismodernragnarok/client/MagicalShotAffixVisuals.java
@@ -4,20 +4,12 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.tacz.guns.client.resource.pojo.display.ammo.AmmoParticle;
 import com.tacz.guns.client.resource.serialize.Vector3fSerializer;
-import com.tacz.guns.entity.EntityKineticBullet;
 import mod.chloeprime.apotheosismodernragnarok.ApotheosisModernRagnarok;
-import mod.chloeprime.apotheosismodernragnarok.common.affix.content.MagicalShotAffix;
-import mod.chloeprime.apotheosismodernragnarok.mixin.tacz.client.AmmoParticleSpawnerAccessor;
-import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.event.TickEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import org.joml.Vector3f;
-
-import java.util.stream.StreamSupport;
 
 @Mod.EventBusSubscriber(Dist.CLIENT)
 public class MagicalShotAffixVisuals {
@@ -40,19 +32,6 @@ public class MagicalShotAffixVisuals {
 
     static {
         MAGIC_PARTICLE.setParticleOptions(ParticleTypes.ELECTRIC_SPARK);
-    }
-
-    @SubscribeEvent
-    public static void bulletTick(TickEvent.LevelTickEvent event) {
-        if (!event.level.isClientSide || event.phase == TickEvent.Phase.END) {
-            return;
-        }
-        var level = (ClientLevel) event.level;
-        StreamSupport.stream(level.entitiesForRendering().spliterator(), false)
-                .filter(EntityKineticBullet.class::isInstance)
-                .map(e -> (EntityKineticBullet)e)
-                .filter(MagicalShotAffix::clientIsMagicBullet)
-                .forEach(ekb -> AmmoParticleSpawnerAccessor.invokeSpawnParticle(ekb, MAGIC_PARTICLE));
     }
 
     public static boolean isMagicGunState;
---
+++ src/main/java/mod/chloeprime/apotheosismodernragnarok/mixin/tacz/client/MagicShotParticleMixin.java
@@ -0,0 +1,28 @@
+package mod.chloeprime.apotheosismodernragnarok.mixin.tacz.client;
+
+import com.tacz.guns.client.particle.AmmoParticleSpawner;
+import com.tacz.guns.client.resource.pojo.display.ammo.AmmoParticle;
+import com.tacz.guns.entity.EntityKineticBullet;
+import mod.chloeprime.apotheosismodernragnarok.client.MagicalShotAffixVisuals;
+import mod.chloeprime.apotheosismodernragnarok.common.affix.content.MagicalShotAffix;
+import net.minecraft.resources.ResourceLocation;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(value = AmmoParticleSpawner.class, remap = false)
+public abstract class MagicShotParticleMixin {
+    @Inject(method = "addParticle", at = @At("HEAD"))
+    private static void spawnMagicShotParticle(EntityKineticBullet bullet, ResourceLocation gunId, CallbackInfo ci) {
+        if (MagicalShotAffix.clientIsMagicBullet(bullet)) {
+            spawnParticle(bullet, MagicalShotAffixVisuals.MAGIC_PARTICLE);
+        }
+    }
+
+    @Shadow
+    private static void spawnParticle(EntityKineticBullet bullet, AmmoParticle particle) {
+        throw new AbstractMethodError();
+    }
+}
