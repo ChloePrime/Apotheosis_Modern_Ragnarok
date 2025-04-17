b7a5eb6f85732146c627272b793d2fce3b6debc3
Chloe Prime <chloe_von_einzbern@qq.com>
2025-04-18 01:01:38 +0800

    专用近战武器击退和火焰附加的兼容

--- src/main/java/mod/chloeprime/apotheosismodernragnarok/common/enchantment/GunEnchantmentHooks.java
+++ src/main/java/mod/chloeprime/apotheosismodernragnarok/common/enchantment/GunEnchantmentHooks.java
@@ -1,16 +1,25 @@
 package mod.chloeprime.apotheosismodernragnarok.common.enchantment;
 
+import com.tacz.guns.api.item.IGun;
 import dev.shadowsoffire.apotheosis.ench.enchantments.masterwork.KnowledgeEnchant;
 import dev.shadowsoffire.apotheosis.ench.enchantments.masterwork.ScavengerEnchant;
 import dev.shadowsoffire.apotheosis.spawn.enchantment.CapturingEnchant;
+import mod.chloeprime.apotheosismodernragnarok.common.affix.category.GunPredicate;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.EnchantmentCategory;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.item.enchantment.Enchantments;
+import net.minecraftforge.event.entity.living.LivingAttackEvent;
+import net.minecraftforge.eventbus.api.EventPriority;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 
 import java.util.Set;
 
+@Mod.EventBusSubscriber
 public class GunEnchantmentHooks {
     private static final ItemStack PREDICATE_SWORD = Items.NETHERITE_SWORD.getDefaultInstance();
     private static final Set<Enchantment> BLACKLIST = Set.of(
@@ -28,5 +37,20 @@ public class GunEnchantmentHooks {
                 || enchantment instanceof ScavengerEnchant
                 || enchantment instanceof KnowledgeEnchant
                 || enchantment instanceof CapturingEnchant;
+    }
+
+    @SubscribeEvent(priority = EventPriority.HIGHEST)
+    public static void fireAspectCompatForMeleeGuns(LivingAttackEvent event) {
+        if (!(event.getSource().getEntity() instanceof LivingEntity attacker)) {
+            return;
+        }
+        var weapon = attacker.getMainHandItem();
+        if (IGun.getIGunOrNull(weapon) == null || !GunPredicate.isMeleeGun(weapon)) {
+            return;
+        }
+        int i = EnchantmentHelper.getFireAspect(attacker);
+        if (i > 0) {
+            event.getEntity().setSecondsOnFire(i * 4);
+        }
     }
 }
--- src/main/java/mod/chloeprime/apotheosismodernragnarok/mixin/tacz/MixinModernKineticGunItem.java
+++ src/main/java/mod/chloeprime/apotheosismodernragnarok/mixin/tacz/MixinModernKineticGunItem.java
@@ -12,6 +12,11 @@ import java.util.List;
 
 @Mixin(value = ModernKineticGunItem.class, remap = false)
 public class MixinModernKineticGunItem {
+    @ModifyVariable(method = "doMelee", at = @At("HEAD"), ordinal = 3, argsOnly = true)
+    private float knockbackEnchantments(float oldKnockback, LivingEntity user) {
+        return oldKnockback + 0.5F * EnchantmentHelper.getKnockbackBonus(user);
+    }
+
     @ModifyVariable(method = "doPerLivingHurt", at = @At("HEAD"), ordinal = 1, argsOnly = true)
     private static float sharpnessEnchantments(float oldDamage, LivingEntity user, LivingEntity target, float knockback, float ignored, List<EffectData> effects) {
         return oldDamage + EnchantmentHelper.getDamageBonus(user.getMainHandItem(), target.getMobType());
